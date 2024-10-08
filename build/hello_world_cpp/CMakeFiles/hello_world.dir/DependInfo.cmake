31783757ac2ade5631891d9872e5f3bf88d252e1
YifeiNie <2096935953@qq.com>
2024-10-08 16:29:20 +0800

    node helloworld

---
+++ build/hello_world_cpp/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nyf/code/ROS2_workspace/hello_world/src/hello_world_cpp/src/hello_world.cpp" "CMakeFiles/hello_world.dir/src/hello_world.cpp.o" "gcc" "CMakeFiles/hello_world.dir/src/hello_world.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/hello_world_cpp/src/hello_world.cpp
+++ src/hello_world_cpp/src/hello_world.cpp
@@ -1,10 +1,12 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
+  rclcpp::init(argc,argv);
+  auto node=rclcpp::Node::make_shared("helloworld_node_cpp");
+  RCLCPP_INFO(node->get_logger(),"hello world");
 
-  printf("hello world hello_world_cpp package\n");
+
+  rclcpp::shutdown();
   return 0;
 }
